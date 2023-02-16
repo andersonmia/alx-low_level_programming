3104b25112e42c620ac267b1608fe357a15ce50c
Majura <andersonmia1968@gmail.com>
2023-02-16 16:51:57 +0200

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
     const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     const int message_len = sizeof(message) - 1;
     write(STDERR_FILENO, message, message_len);
