55114c2923963a3e5ed2ea25bafbdeaee9141679
Majura <andersonmia1968@gmail.com>
2023-02-16 17:10:22 +0200

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,12 @@
  */
 int main(void)
 {
-    const char message[] =
-        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    const int message_len = sizeof(message) - 1;
-    write(STDERR_FILENO, message, message_len);
-    return (1);
+const char message[] =
+    "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+const int message_len = sizeof(message) - 1;
+
+write(STDERR_FILENO, message, message_len);
+
+return (1);
 }
