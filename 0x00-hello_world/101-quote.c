83ce2fc7bdffe99de7f2a14823f4b9b8a3d35494
Majura <andersonmia1968@gmail.com>
2023-02-16 16:51:24 +0200

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
 int main(void) {
-    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return (0);
+    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    const int message_len = sizeof(message) - 1;
+    write(STDERR_FILENO, message, message_len);
+    return (1);
 }
