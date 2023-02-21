2f23a3d4a2d6d51de667f10b9cd0cea2d3c1b30a
Majura <andersonmia1968@gmail.com>
2023-02-21 17:36:40 +0200

    0x02-functions_nested_loops/100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,55 +1,44 @@
 #include "main.h"
 
 /**
- * print_times_table - prints times table for numbers from 0-14
+ * print_times_table - prints times table for counterbers from 0-14
  * @n: An input integer value
  * Return: Nothing
  */
 void print_times_table(int n)
 {
-	int i, j;
+	int counter, var2, var3;
 
-	if (n > 0 && n < 15)
+	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)
+		for (counter = 0; counter <= n; counter++)
 		{
 			_putchar('0');
-			for (j = 1; j <= n; j++)
-				putformat(i * j);
+
+			for (var2 = 1; var2 <= n; var2++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				var3 = counter * var2;
+
+				if (var3 <= 99)
+					_putchar(' ');
+				if (var3 <= 9)
+					_putchar(' ');
+
+				if (var3 >= 100)
+				{
+					_putchar((var3 / 100) + '0');
+					_putchar(((var3 / 10)) % 10 + '0');
+				}
+				else if (var3 <= 99 && var3 >= 10)
+				{
+					_putchar((var3 / 10) + '0');
+				}
+				_putchar((var3 % 10) + '0');
+			}
 			_putchar('\n');
 		}
-	}
-}
-
-/**
- * putformat - formatted characters to output
- * @n: number to format
- * Return: nothing
- */
-void putformat(int n)
-{
-	if (n <= 9)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
 	}
 }
