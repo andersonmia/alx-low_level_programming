c6ba440d7acd87617d40ff128c3136193e6e45cb
Majura Anthony Gaudence <andersonmia1968@gmail.com>
2023-03-06 17:51:40 +0200

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,8 +4,9 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-    for(unsigned int i = 0; i < n; i++)
-        s[i] = b;
-    
-    return (s);
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
